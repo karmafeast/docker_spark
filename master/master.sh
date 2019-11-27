bde866a524ef78868db76bddb364929211d6c9d9
Leo Coulson <lcoulson@bloomberglaw.com>
2019-11-27 11:31:53 -0500

    history server on master, shared log dir, set snappy rdd serialize codec
    history server on master, shared log dir, set snappy rdd serialize codec

--- master/master.sh
+++ master/master.sh
@@ -14,5 +14,7 @@ ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 /usr/local/spark/sbin/start-shuffle-service.sh
 
+/usr/local/spark/sbin/start-history-server.sh
+
 cd /usr/local/spark/bin && /usr/local/spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
